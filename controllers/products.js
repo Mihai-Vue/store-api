0c69cc89f436c1ce4b746a6fdde56730bd318990
Mihai Boureanu <boureanumihai@yahoo.com>
2024-07-25 21:56:59 +0300

    added find/sort/select products function

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,54 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products= await Product.find({featured: true});
-  res.status(200).json({products, nbHits: products.length});
+  const products = await Product.find({})
+    .sort("name")
+    .select("name price")
+    .limit(10)
+    .skip(1);
+
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products testing route" });
+  const { featured, company, name, sort, fields } = req.query;
+  const queryObj = {};
+
+  if (featured) {
+    queryObj.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObj.company = company;
+  }
+  if (name) {
+    queryObj.name = { $regex: name, $options: "i" };
+  }
+  // console.log(queryObj);
+
+  let result = Product.find(queryObj);
+
+  //sort
+  if (sort) {
+    // products = products.sort();
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  //fields
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(res.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+
+  const products = await result;
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
