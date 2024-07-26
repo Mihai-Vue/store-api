e71d807e4424142be5b5197e0060f2cec1c7bf1b
Mihai Boureanu <boureanumihai@yahoo.com>
2024-07-26 15:02:53 +0300

    implemented numeric filters

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,9 @@
 const Product = require("../models/product");
+const { options } = require("../routes/products");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({})
-    .sort("name")
+  const products = await Product.find({ price: { $gt: 30, $lt: 100 } })
+    .sort("price")
     .select("name price")
     .limit(10)
     .skip(1);
@@ -11,7 +12,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObj = {};
 
   if (featured) {
@@ -23,6 +24,34 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObj.name = { $regex: name, $options: "i" };
   }
+  if (numericFilters) {
+    console.log(numericFilters);
+
+    //mapping the oprators into mongoose style :)
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "<": "$lt",
+      "<=": "$lte",
+      "=": "$eq",
+    };
+    const regEX = /\b(<|>|>=|<= |=)\b/g;
+    let filters = numericFilters.replace(
+      regEX,
+      (match) => `-${operatorMap[match]}-`,
+    );
+    //console.log(filters);
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObj[field] = { [operator]: Number(value) };
+      }
+    });
+    console.log(options);
+    console.log(queryObj);
+  }
+
   // console.log(queryObj);
 
   let result = Product.find(queryObj);
@@ -43,8 +72,9 @@ const getAllProducts = async (req, res) => {
   }
 
   const page = Number(req.query.page) || 1;
-  const limit = Number(res.query.limit) || 10;
+  const limit = Number(req.query.limit) || 10;
   const skip = (page - 1) * limit;
+
   result = result.skip(skip).limit(limit);
 
   const products = await result;
